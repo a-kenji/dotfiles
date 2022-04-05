857b82f199c09b42d2c88dcfb672d338cfa3305b
a-kenji <aks.kenji@protonmail.com>
2022-04-05 11:45:33 +0200

    fmt

--- flake.nix
+++ flake.nix
@@ -2,11 +2,10 @@
   description = "Dotfiles";
 
   inputs = {
-     flake-utils.url = "github:numtide/flake-utils";
-     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-     home-manager.url = "github:nix-community/home-manager";
-     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = {...} @ args: import ./outputs.nix args;
 }
--- outputs.nix
+++ outputs.nix
@@ -3,26 +3,26 @@
   flake-utils,
   nixpkgs,
   ...
-} @ inputs:
-(flake-utils.lib.eachDefaultSystem (system: let
-  pkgs = nixpkgs.legacyPackages.${system};
-in {
-   devShells = {
-    shell = pkgs.mkShell {
-      nativeBuildInputs = [
-      ];
+} @ inputs: (flake-utils.lib.eachDefaultSystem (
+  system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells = {
+      shell = pkgs.mkShell {
+        nativeBuildInputs = [
+        ];
+      };
+      fmtShell = pkgs.mkShell {
+        name = "fmt-shell";
+        nativeBuildInputs = [
+          pkgs.treefmt
+          pkgs.stylua
+          pkgs.alejandra
+        ];
+      };
     };
-    fmtShell = pkgs.mkShell {
-      name = "fmt-shell";
-      nativeBuildInputs = [
-        pkgs.treefmt
-        pkgs.stylua
-        pkgs.alejandra
-      ];
-    };
-  };
 
-  #devShell = pkgs.callPackage ./shell.nix {
-  #};
-}
-  ))
+    #devShell = pkgs.callPackage ./shell.nix {
+    #};
+  }
+))
