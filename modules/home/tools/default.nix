8d511a87d340d4a7a7dd7cc969f190fe3c7dd9b6
a-kenji <aks.kenji@protonmail.com>
2022-05-18 23:35:42 +0200

    add: git

--- modules/home/tools/default.nix
+++ modules/home/tools/default.nix
@@ -3,5 +3,17 @@
   configDir,
   ...
 }: let
+  git = [
+    git
+    gitAndTools.gh # github cli
+    hub
+    gitui
+    tig
+    lazygit
+    git-absorb
+    difftastic
+    delta
+  ];
 in {
+  home.packages = git;
 }
