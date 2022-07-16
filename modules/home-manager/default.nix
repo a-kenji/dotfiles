b2e6faca4f1bf923f69f8aee2c851dcbbfbbc92d
a-kenji <aks.kenji@protonmail.com>
2022-07-16 20:12:00 +0200

    chore(update): home-manager -> `22.11`
    The Flake function `homeManagerConfiguration` has been simplified.
    
    Release Notes:
    
    - https://nix-community.github.io/home-manager/release-notes.html#sec-release-22.11

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -5,21 +5,26 @@
   ...
 }: let
   hmConfiguration = {
-    extraModules ? [],
+    modules ? [],
     system ? "x86_64-linux",
+    stateVersion ? "21.11",
+    homeDirectory ? "/home/kenji",
+    username ? "kenji",
+    pkgs ? nixpkgs.legacyPackages.${system},
   }: (home-manager.lib.homeManagerConfiguration {
-    configuration = {...}: {
-      imports =
-        extraModules
-        ++ [
-          self.outputs.nixosModules.home.nvim
-        ];
-    };
-    inherit system;
-    homeDirectory = "/home/kenji";
-    username = "kenji";
+    inherit pkgs;
+    modules =
+      modules
+      ++ [
+        self.outputs.nixosModules.home.nvim
+        {
+          home = {
+            inherit stateVersion homeDirectory username;
+          };
+        }
+      ];
   });
 in {
   common = hmConfiguration {};
-  tools = hmConfiguration {extraModules = [self.outputs.nixosModules.home.tools];};
+  tools = hmConfiguration {modules = [self.outputs.nixosModules.home.tools];};
 }
