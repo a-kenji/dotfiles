d63615612077d891df4f4ea2bb7c74ae33532547
a-kenji <aks.kenji@protonmail.com>
2022-09-03 23:43:53 +0200

    add follows attribute for flake-utils

--- flake.nix
+++ flake.nix
@@ -6,8 +6,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.inputs.utils.follows = "flake-utils";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    neovim-nightly-overlay.inputs.utils.follows = "flake-utils";
     nvim-surround.url = "github:kylechui/nvim-surround";
     nvim-surround.flake = false;
   };
