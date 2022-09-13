97d6712c24a5750c0fe967a530b32a1358e543ce
a-kenji <aks.kenji@protonmail.com>
2022-09-13 15:10:10 +0200

    add: nix injections

--- home/nvim/queries/nix/injections.scm
+++ home/nvim/queries/nix/injections.scm
@@ -5,3 +5,68 @@ attrpath: (attrpath attr: (identifier) @_test )
 expression: (indented_string_expression) @python
 (#eq? @_test "testScript")
 )
+
+; inject bash into runCommand
+(
+apply_expression
+function:  (apply_expression 
+function: (apply_expression 
+function: (variable_expression 
+name: (identifier) @_test))) 
+argument: (indented_string_expression) @bash
+(#eq? @_test "runCommand")
+)
+
+; inject bash into buildPhases
+(
+binding
+attrpath: (attrpath attr: (identifier) @_test)
+expression: (indented_string_expression) @bash
+(#any-of? @_test 
+"unpackPhase" "preUnpack" "postUnpack"
+"patchPhase" "prePatch" "postPatch"
+"configurePhase" "preConfigure" "postConfigure"
+"buildPhase" "preBuild" "postBuild"
+"checkPhase" "preCheck" "postCheck"
+"installPhase" "preInstall" "postInstall" 
+"fixupPhase" "preFixup" "postFixup"
+"installCheckPhase" "preInstallCheck" "postInstallCheck" 
+"distPhase" "preDist" "postDist" 
+)
+) 
+; a different variation: TODO can this be merged?
+(
+binding
+attrpath: 
+(attrpath attr: (identifier) @_test)
+expression: (binary_expression) @bash
+(#any-of? @_test 
+"unpackPhase" "preUnpack" "postUnpack"
+"patchPhase" "prePatch" "postPatch"
+"configurePhase" "preConfigure" "postConfigure"
+"buildPhase" "preBuild" "postBuild"
+"checkPhase" "preCheck" "postCheck"
+"installPhase" "preInstall" "postInstall" 
+"fixupPhase" "preFixup" "postFixup"
+"installCheckPhase" "preInstallCheck" "postInstallCheck" 
+"distPhase" "preDist" "postDist" 
+)
+) 
+(
+binding
+attrpath: (attrpath attr: (identifier) @_test)
+expression: (apply_expression 
+function: (apply_expression)
+argument: (indented_string_expression) @bash)
+(#any-of? @_test 
+"unpackPhase" "preUnpack" "postUnpack"
+"patchPhase" "prePatch" "postPatch"
+"configurePhase" "preConfigure" "postConfigure"
+"buildPhase" "preBuild" "postBuild"
+"checkPhase" "preCheck" "postCheck"
+"installPhase" "preInstall" "postInstall" 
+"fixupPhase" "preFixup" "postFixup"
+"installCheckPhase" "preInstallCheck" "postInstallCheck" 
+"distPhase" "preDist" "postDist" 
+)
+) 
