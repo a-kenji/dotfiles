44ab90ed5c18109981a0eabdd277120571bfda3c
a-kenji <aks.kenji@protonmail.com>
2022-09-23 21:17:31 +0200

    chore(fmt): treefmt

--- modules/tests/neovim.nix
+++ modules/tests/neovim.nix
@@ -1,8 +1,7 @@
-{ pkgs
-, self
-,
-}:
-let
+{
+  pkgs,
+  self,
+}: let
   system = "x86_64-linux";
   home = "/home/${user}";
   servername = "${home}/nvim-socket";
@@ -10,12 +9,12 @@ let
   nvimBin = "${home}/.nix-profile/bin/nvim";
   user = "alice";
 in
-pkgs.nixosTest {
-  nodes.machine =
-    { config
-    , pkgs
-    , lib
-    , ...
+  pkgs.nixosTest {
+    nodes.machine = {
+      config,
+      pkgs,
+      lib,
+      ...
     }: {
       imports = [
         self.inputs.home-manager.nixosModules.home-manager
@@ -42,30 +41,30 @@ pkgs.nixosTest {
       documentation.enable = false;
     };
 
-  testScript = ''
-    from shlex import quote
-    def su(user, cmd):
-        return f"su - {user} -c {quote(cmd)}"
+    testScript = ''
+      from shlex import quote
+      def su(user, cmd):
+          return f"su - {user} -c {quote(cmd)}"
 
-    start_all()
-    machine.wait_for_file("${nvimBin}")
-    machine.succeed(
-    su('${user}', '${nvimBin} --version')
-    )
-    machine.execute(
-    su('${user}', '${nvimBin} --listen ${servername} --headless >&2 &')
-    )
-    machine.succeed(
-    su('${user}', '${nvimBin} --server ${servername} --remote-send "<cmd>checkhealth<CR>"')
-    )
-    machine.succeed(
-    su('${user}', '${nvimBin} --server ${servername} --remote-send "<cmd>w ${checkfile}<CR>"')
-    )
-    machine.wait_for_file("${checkfile}")
-    machine.succeed(
-    su('${user}', 'cat ${checkfile}')
-    )
-    machine.succeed("cat ${home}/.config/nvim/init.vim")
-    machine.succeed("cat ${home}/.config/nvim/lua/init.lua")
-  '';
-}
+      start_all()
+      machine.wait_for_file("${nvimBin}")
+      machine.succeed(
+      su('${user}', '${nvimBin} --version')
+      )
+      machine.execute(
+      su('${user}', '${nvimBin} --listen ${servername} --headless >&2 &')
+      )
+      machine.succeed(
+      su('${user}', '${nvimBin} --server ${servername} --remote-send "<cmd>checkhealth<CR>"')
+      )
+      machine.succeed(
+      su('${user}', '${nvimBin} --server ${servername} --remote-send "<cmd>w ${checkfile}<CR>"')
+      )
+      machine.wait_for_file("${checkfile}")
+      machine.succeed(
+      su('${user}', 'cat ${checkfile}')
+      )
+      machine.succeed("cat ${home}/.config/nvim/init.vim")
+      machine.succeed("cat ${home}/.config/nvim/lua/init.lua")
+    '';
+  }
