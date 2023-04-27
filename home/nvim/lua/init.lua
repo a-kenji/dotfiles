9cb0422ab8a2d466b9ab910866318405e1854643
a-kenji <aks.kenji@protonmail.com>
2023-04-27 19:57:47 +0200

    init: copilot

--- home/nvim/lua/init.lua
+++ home/nvim/lua/init.lua
@@ -172,3 +172,52 @@ end, { desc = "Next todo comment" })
 vim.keymap.set("n", "[t", function()
 	require("todo-comments").jump_prev()
 end, { desc = "Previous todo comment" })
+require("glow").setup()
+
+require("copilot").setup({
+	require("copilot").setup({
+		panel = {
+			enabled = true,
+			auto_refresh = false,
+			keymap = {
+				jump_prev = "[[",
+				jump_next = "]]",
+				accept = "<CR>",
+				refresh = "gr",
+				open = "<M-CR>",
+			},
+			layout = {
+				position = "right", -- | top | left | right
+				ratio = 0.3,
+			},
+		},
+		suggestion = {
+			enabled = true,
+			auto_trigger = false,
+			debounce = 75,
+			keymap = {
+				accept = "<M-l>",
+				accept_word = false,
+				accept_line = false,
+				next = "<M-]>",
+				prev = "<M-[>",
+				dismiss = "<C-]>",
+			},
+		},
+		filetypes = {
+			rust = true,
+			nix = true,
+			yaml = false,
+			markdown = false,
+			help = false,
+			gitcommit = false,
+			gitrebase = false,
+			hgcommit = false,
+			svn = false,
+			cvs = false,
+			["."] = false,
+		},
+		copilot_node_command = "node", -- Node.js version must be > 16.x
+		server_opts_overrides = {},
+	}),
+})
