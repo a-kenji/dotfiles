978513ce98949b0087edc3e66624321fc09c14ec
a-kenji <aks.kenji@protonmail.com>
2023-06-13 19:58:03 +0200

    nvim(fix): typo

--- home/nvim/lua/kenji/lsp/init.lua
+++ home/nvim/lua/kenji/lsp/init.lua
@@ -183,7 +183,7 @@ end
 
 local diagnostics_active = true
 local toggle_diagnostics = function()
-	diagnostics_active = not diagnostivs_active
+	diagnostics_active = not diagnostics_active
 	if diagnostics_active then
 		vim.diagnostic.show()
 	else
